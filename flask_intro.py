0b07f451413540cd98e361003b087e9a4ed9b77e
Talha-Aslam <ta104g38@gmail.com>
2025-09-15 19:27:43 +0500

    asd

--- flask_intro.py
+++ flask_intro.py
@@ -8,4 +8,4 @@ def hello_world():
 
 # Run the application
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the application in debug mode
+    app.run(debug=True)  # Run the application in debug mode    
